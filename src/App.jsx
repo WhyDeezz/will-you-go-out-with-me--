8422d31e20bcd58a2d0ce582ff2ad4b1bc259c98
Vaithiesh Jayasankar <vaithieshjsankar@gmail.com>
2025-10-05 08:13:10 +0530

    uploading projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import No from "./assets/Components/No";
 import Yes from "./assets/Components/Yes";
+import car from "./assets/Components/car.png"
 
 export default function App()
 {
   return(
     <>
     <div className="catparent">
-      <img src='./car.png' className="car"></img>
+      <img src={car} className="car"></img>
     </div>
     <div className="parent">
       <h1 className="title">Will You goo out with me 🥺👉👈 ???!!</h1>
